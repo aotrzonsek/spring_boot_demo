b9981848d4a8e468bfdcc1f4af76142888a4b9b7
Artur Otrzonsek <artur.otrzonsek@capgemini.com>
2014-11-12 09:04:24 +0100

    junit tests added

---
+++ src/test/java/akai/UserServiceMockTest.java
@@ -0,0 +1,69 @@
+package akai;
+
+import akai.jpa.User;
+import akai.repository.UserRepository;
+import akai.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class UserServiceMockTest {
+    @Mock
+    private UserRepository userRepository;
+    @InjectMocks
+    private UserService userService;
+
+    @Before
+    public void init() {
+        userService = new UserService();
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testShouldCallUserRepositoryFindByName() {
+        // given
+        final String userLastName = "userLastName";
+        // when
+        userService.findUserByName(userLastName);
+        // then
+        verify(userRepository).findByName(userLastName);
+    }
+
+    @Test
+    public void testShouldDeleteUser() {
+        // given
+        final long userIdToDelete = 1L;
+        final User user = mock(User.class);
+        when(userRepository.findOne(anyLong())).thenReturn(user);
+        when(user.isActive()).thenReturn(true);
+        // when
+        userService.deleteUserById(userIdToDelete);
+        // then
+        ArgumentCaptor<Long> longArgumentCaptor = ArgumentCaptor.forClass(Long.class);
+        verify(userRepository).findOne(longArgumentCaptor.capture());
+        assertEquals(userIdToDelete, longArgumentCaptor.getValue().longValue());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testShouldThrowValidationExceptionOnDeleteUser() {
+        // given
+        final long userIdToDelete = 1L;
+        final User user = mock(User.class);
+        when(userRepository.findOne(anyLong())).thenReturn(user);
+        when(user.isActive()).thenReturn(false);
+        // when
+        userService.deleteUserById(userIdToDelete);
+        // then
+        fail("test should fail there");
+    }
+}
---
+++ src/test/java/akai/UserServiceTest.java
@@ -0,0 +1,33 @@
+package akai;
+
+import akai.jpa.User;
+import akai.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+	@Test
+	public void testShouldFindUserByName() {
+        // given
+        final String userName = "Kowalski";
+        // when
+        User user = userService.findUserByName(userName);
+        //then
+        assertNotNull(user);
+        assertEquals(userName, user.getLastName());
+	}
+}
